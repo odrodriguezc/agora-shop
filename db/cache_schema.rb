04dc6a41353d894481a8c4f26005a094838b696c
odrc <odrodriguezc@gmail.com>
2025-09-15 10:08:28 +0200

    Remove file from bad branch merge Homogenize cache db schema add test to has public uuid module

--- db/cache_schema.rb
+++ db/cache_schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_11_213922) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_12_181411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
+
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "uuid", default: -> { "gen_random_uuid()" }, null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+    t.index ["uuid"], name: "index_sessions_on_uuid", unique: true
+  end
 
   create_table "solid_cache_entries", force: :cascade do |t|
     t.binary "key", null: false
@@ -25,4 +36,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_11_213922) do
     t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
     t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "uuid", default: -> { "gen_random_uuid()" }, null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["uuid"], name: "index_users_on_uuid", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
